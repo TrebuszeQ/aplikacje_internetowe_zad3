73c5077d5b74c99d42806489f14660a2fe721022
admin <trebuszeq@gmail.com>
2025-03-17 22:27:52 +0100

    init

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <title>Zadanie3</title>
+    <head>
+        <meta name="description" content="">
+        <meta name="keywords" content="HTML, CSS, PHP">
+        <meta name="author" content="Hubert Dabrowski">
+        <meta charset="utf-8">
+        <meta name="viewport" context="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="./w3.css">
+    </head>
+    <body>
+        <?php
+            echo "Hello World";
+        ?>
+        <header>
+        </header>
+        <navbar>
+        </navbar>
+        <footer>
+        </footer>
+    </body>
+</html>
